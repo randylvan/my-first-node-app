9f4f06653a836efaf4fd67e8ccc6f9047ed42446
Randy Van <randyvanprojets@gmail.com>
2017-01-31 20:33:05 -0700

    error handling and index html

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var http = require('http');
-var port = '8080';
+var fs = require('fs');
+
+var port = 8080;
 
 http.createServer(function (request, response){
-    response.writeHead(200,{'Content-Type': 'text/plain'});
-    response.end("My first node app");
+    //read in file
+    fs.readFile('index.html', function(err,content){
+        if(err){
+            response.writeHead(404,{'Content-Type': 'text/html'});
+            response.end('failed');
+        }
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.end(content);
+    });
 }).listen(port);
 
 //Tell the server it started
-console.log('server rnning on port' + port);
+console.log('server running on port ' + port);
 
+//Non - Blocking Code
